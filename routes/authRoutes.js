8dd974f9b147df65991c5d506b60595155ef6594
Hasanboy Rasuljonov <107887755+rasuljonov@users.noreply.github.com>
2024-04-21 11:38:59 +0500

    Inital commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { register, login } = require('../controllers/authController');
+const router = express.Router();
+
+// Define the registration route
+router.post('/register', register);
+router.post('/login', login);
+router.get('/dashboard')
+
+
+module.exports = router;
