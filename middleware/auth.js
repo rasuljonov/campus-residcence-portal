1016cd8ded3e58eb7de4e46d013adf934cfaeeff
Hasanboy Rasuljonov <107887755+rasuljonov@users.noreply.github.com>
2024-05-22 23:08:21 +0500

    minor changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,29 @@
 
-
-    const jwt = require('jsonwebtoken');
-    const { APP } = require('../config'); // Ensure APP config includes SECRET_KEY
+    const jwt = require('jsonwebtoken')
+    const { APP } = require('../config');
+const { request } = require('express');
 
     // Middleware to authenticate token and attach user to request
     const authenticateToken = (req, res, next) => {
         const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1]; // Bearer Token
+        const token = authHeader && authHeader.split(''[1]);
 
         if (!token) {
-            return res.status(401).json({ message: "No token provided, authorization denied." });
+            return res.status(401).json({ message: 'No token provided, authorization denied'});
         }
 
         jwt.verify(token, APP.SECRET_KEY, (err, decoded) => {
             if (err) {
-                return res.status(403).json({ message: "Token is not valid." });
+                return res.status(403).json({message: 'Token is not valid'})
             }
 
-            req.user = decoded; // Attach decoded token to request
-            console.log('Hello', req.user)
-            next();
+            req.user = decoded
+
+            next()
         });
-    };
+
+    
+    }
 
     // Middleware to check user role
     const checkRole = (roleRequired) => (req, res, next) => {
